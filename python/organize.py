46ba044742ef7b34180010a47217d3e0c0d8b185
slashroot305 <iamjovybz@gmail.com>
2025-12-16 00:39:05 -0600

    chnaged organize script from sh to py

---
+++ python/organize.py
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ====================================================================
+# Script Name: organize.py
+# Description: Organizes all files in a directory by type on all OSes
+# Author:
+# ====================================================================
