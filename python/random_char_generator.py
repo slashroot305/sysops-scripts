c35493375abfa076db493f0fe6c5c97b35e2dc85
slashroot305 <iamjovybz@gmail.com>
2026-02-23 18:51:17 -0600

    add special characters to password

--- python/random_char_generator.py
+++ python/random_char_generator.py
@@ -4,9 +4,12 @@ import random, string, secrets, tempfile, subprocess, platform, os
 
 # generate secure random 16 character string
 random_string = ''.join(
-    secrets.choice(
-        string.ascii_letters + string.digits)
+    secrets.choice(string.ascii_letters + string.digits + string.punctuation)
         for _ in range(16))
+
+print(random_string)
+
+
 
 '''''
 # temp file with results that won't be saved
@@ -14,8 +17,6 @@ with tempfile.NamedTemporaryFile(mode='w', suffix='txt', delete=False) as temp_f
     temp_file.write(random_string)
     temp_file_path = temp_file.name
 '''''
-
-print(random_string)
 
 ''''
 try:
